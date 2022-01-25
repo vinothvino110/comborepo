350b2add18ad5d733d4b76923121bcedc6d1deef
vinoth.l <vinothsquare110@gmail.com>
2022-01-25 16:22:34 +0530

    1 st commit

---
+++ examplecheck/src/main/java/examplecheck/constants/ExamplecheckPortletKeys.java
@@ -0,0 +1,11 @@
+package examplecheck.constants;
+
+/**
+ * @author vinoth.l
+ */
+public class ExamplecheckPortletKeys {
+
+	public static final String EXAMPLECHECK =
+		"examplecheck_ExamplecheckPortlet";
+
+}
---
+++ examplecheck/src/main/resources/META-INF/resources/view.jsp
@@ -0,0 +1,7 @@
+<%@ include file="/init.jsp" %>
+
+<p>
+	<b><liferay-ui:message key="examplecheck.caption"/></b>
+</p>
+
+Working dude
